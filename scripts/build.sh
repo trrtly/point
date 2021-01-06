83d20985534cd267a659fe248524e71daf6d6d2b
wenlong <328602875@qq.com>
2021-01-06 12:38:40 +0800

    chore: update

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-echo "building docker images for ${GOOS}/${GOARCH} ..."
-
 # compile the server using the cgo
 CGO_ENABLED=0 GOOS=linux go build -ldflags="-w -s" -a -installsuffix cgo -o release/point-server ./cmd/server
